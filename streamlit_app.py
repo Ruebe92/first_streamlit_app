58da7b8124282c1ad599a89d023c9e8406189d66
Christoph Lenk <christoph-lenk@web.de>
2023-03-10 09:42:30 +0100

    Update streamlit_app.py
    added requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+import requests
 import streamlit
 import pandas
 
@@ -19,3 +20,9 @@ streamlit.dataframe(my_fruit_list)
 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
+
+streamlit.header("Fruityvice Fruit Advice!")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
+## Food-API-Key: 1dkQOdxq2n1nH09C3cjzexdiGvzOd0UcGmuWlK0T 
