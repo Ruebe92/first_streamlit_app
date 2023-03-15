e4b5e3a246449dd4019d5d47dfd1e41ce710d3fd
Christoph Lenk <christoph-lenk@web.de>
2023-03-15 12:08:55 +0100

    Update streamlit_app.py
    text input added

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,5 +43,7 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load contains:")
 streamlit.dataframe(my_data_rows)
 
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 
+streamlit.write('Thanks for adding', fruit_choice)
 ## Food-API-Key: 1dkQOdxq2n1nH09C3cjzexdiGvzOd0UcGmuWlK0T 
