a344f7bf27e6b7de6c5ca8d4e68947f5f9c05d6a
Christoph Lenk <christoph-lenk@web.de>
2023-03-15 11:47:51 +0100

    Update streamlit_app.py
    Added snowflake-connector test

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,13 @@ import streamlit
 import pandas
 import snowflake.connector
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
 streamlit.title("MY Mom's New Healthy Diner")
 streamlit.text("🥝🍇🍈 Omega 3  Blueberry Oatmeal")
 streamlit.text("🍌🍑🍉Kale, Spinach & Rocket Smoothie")
